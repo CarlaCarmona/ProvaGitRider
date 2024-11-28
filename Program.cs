53ec35445cbcfeb0441f1a6f16b7d051111b23dd
Carla <carlacarmona9@gmail.com>
2024-11-28 19:05:52 +0100

    Versio Base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProvaGitRider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
